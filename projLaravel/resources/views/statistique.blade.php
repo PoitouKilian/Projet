8b98bdd30d9a163ff0bec119d74ef6a3ce232695
kpoitou <kpoitou@pommier4.depinfo.touchard.edu>
2019-05-07 11:03:00 +0200

    ajout

--- projLaravel/resources/views/statistique.blade.php
+++ projLaravel/resources/views/statistique.blade.php
@@ -1,5 +1,58 @@
 @extends('layouts.app')
+<!-- Style page -->
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
 @section('content')
 <h1>Statistique</h1> 
+
+
+
+<img src="IMAGES/diagramme.jpg" style="width: 100%; margin-left: 30%;">
+<br>
+<br>
+<br>
+<div class="container">
+ <table class="table table-bordered">
+            <thead class="titreRapport">
+                <tr>
+                    <th>Nombre d'erreur dans chaque ronde</th>
+                    <th>Nombre d'erreur en fonction de l'agent</th>
+                    <th>Nombre d'erreur en fonction de la ronde</th>
+                    <th>Nombre de ronde</th>
+                    <th>Nombre de ronde avec erreur</th>
+                </tr>
+            </thead>
+            <tbody> 
+                    <tr> 
+                        <td><input type="button" value="Selectionnez"></td>
+                        <td><input type="button" value="Selectionnez"></td>
+                        <td><input type="button" value="Selectionnez"></td>
+                        <td><input type="button" value="Selectionnez"></td>       
+                        <td><input type="button" value="Selectionnez"></td>
+                    </tr> 
+            </tbody>
+        </table>   
+    
+    <table class="table table-bordered">
+            <thead class="titreRapport">
+                <tr>
+                    <th>Nombre de ronde correct</th>
+                    <th>Ronde qui à généré le plus d'erreur</th>
+                    <th>Ronde qui à généré le plus de retard</th>
+                    <th>Ronde qui à généré le plus d'avance</th>
+                </tr>
+            </thead>
+            <tbody> 
+                    <tr> 
+                        <td><input type="button" value="Selectionnez"></td>
+                        <td><input type="button" value="Selectionnez"></td>
+                        <td><input type="button" value="Selectionnez"></td>
+                        <td><input type="button" value="Selectionnez"></td>
+                    </tr> 
+            </tbody>
+        </table>   
+</div>
 @endsection
